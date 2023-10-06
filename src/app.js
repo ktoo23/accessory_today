0d06cfbb0db3c5d63d494f8bf50db86efc936834
[SW6] 유수빈 <syubim@gmail.com>
2023-10-06 23:44:52 +0900

    브랜치 만드는거 깜밖해서 뭘 좀 적었어요...

--- src/app.js
+++ src/app.js
@@ -13,21 +13,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/", {
+  .connect("mongodb://127.0.0.1:27017/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("mongoDB 연결에 성공하였습니다😊"))
   .catch((err) => console.log("mongoDB 연결에 실패하였습니다😥" + err));
 
-
-app.get('/api', async (req, res) => {
-    const bestData= await homeService.getBest();
-    const newData= await homeService.getNew();
-    res.json([bestData,newData]);
+app.get("/api", async (req, res) => {
+  const bestData = await homeService.getBest();
+  const newData = await homeService.getNew();
+  res.json([bestData, newData]);
 });
 
 app.use("/api/users", userRouter);
 app.use("/", categoryRouter);
-app.use("/api/products",productRouter);
+app.use("/api/products", productRouter);
 export { app };
