9bf3951a147ad830eb8f962017a779947b5a5247
[SW6] 유수빈 <syubim@gmail.com>
2023-10-06 23:47:22 +0900

    관리자 상품등록 중..

---
+++ src/routers/adminRouter.js
@@ -0,0 +1,4 @@
+import { Router } from "express";
+import { productService } from "../services/productService.js";
+import { reviewService } from "../services/reviewService.js";
+import { inquiryService } from "../services/inquiryService.js";
