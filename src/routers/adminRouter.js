0ec248956041975e5d96e41b841ac7365bc380a5
[SW6] 유수빈 <syubim@gmail.com>
2023-10-07 01:17:48 +0900

    관리자 상품 수정 구현. 상품이 있을 시 수정 가능, 상품이 없을 시 에러메세지 없는 버그 발생

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -64,4 +64,37 @@ adminRouter.delete("/products", async (req, res) => {
   }
 });
 
+// 관리자 상품 수정
+adminRouter.patch("/products", async (req, res) => {
+  // 요청으로 들어온 내용들 구조 분해 할당
+  const {
+    id,
+    productName,
+    price,
+    category,
+    description,
+    isNew,
+    isBest,
+    productImg,
+  } = req.body;
+  try {
+    // 상품 개설 요청 처리 결과를 받음. (상태 코드 200이면 성공)
+    const patchProductResult = await adminService.patchProduct(
+      id,
+      productName,
+      price,
+      category,
+      description,
+      isNew,
+      isBest,
+      productImg
+    );
+    return res.json(patchProductResult);
+  } catch (err) {
+    return res.json(err);
+  }
+});
+
+//const targetId = new ObjectId(Id);//ObjectId형태로 변환
+
 export { adminRouter };
--- src/services/adminService.js
+++ src/services/adminService.js
@@ -71,6 +71,50 @@ class AdminService {
       productName: productName,
     };
   }
+
+  //관리자 상품 수정
+  async patchProduct(
+    id,
+    productName,
+    price,
+    category,
+    description,
+    isNew,
+    isBest,
+    productImg
+  ) {
+    // 중복되는 제품이 존재하는지 검사
+    // Convert the id to ObjectId
+    const targetId = new ObjectId(id); // ObjectId형태로 변환
+
+    // Check if the product exists
+    const productExsist = await Products.findById(targetId); // id로 찾기
+    //중복되는 상품이 없을 경우 존재하지 않는 상품이라고 알리기
+    if (!productExsist) {
+      return {
+        status: 404,
+        errMsg: "존재하지 않는 상품입니다.",
+      };
+    }
+    // db에 저장하기
+    await Products.findOneAndUpdate(
+      { _id: targetId },
+      {
+        productName,
+        price,
+        category,
+        description,
+        isNew,
+        isBest,
+        productImg,
+      }
+    );
+    // 성공 메시지
+    return {
+      status: 200,
+      message: "상품 정보를 수정했습니다",
+    };
+  }
 }
 
 //
