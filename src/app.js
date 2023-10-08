9abe35db9acbcaeac59510ab638dfa0139e2f878
YuBeenSo <yso682879@gmail.com>
2023-10-08 18:49:56 +0900

    orderrouter 빠뜨림..

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { userRouter } from "./routers/userRouter.js";
+import { orderRouter } from "./routers/orderRouter.js";
 import { categoryRouter } from "./routers/categoryRouter.js";
 import { productRouter } from "./routers/productsRouter.js";
 import { adminRouter } from "./routers/adminRouter.js";
