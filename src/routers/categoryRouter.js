ca04e07e66064bb44f80f76247cf429a55db1625
[SW6] 유수빈 <syubim@gmail.com>
2023-10-09 17:51:15 +0900

    카테고리 라우터 수정

--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -7,7 +7,7 @@ const categoryRouter = Router();
 // 새 카테고리
 categoryRouter.post("/categorys", async (req, res) => {
   // 요청으로 들어온 내용들 구조 분해 할당
-  const { category } = req.body;
+  const { newCategory } = req.body;
   try {
     // 카테고리 개설 요청 처리 결과를 받음. (상태 코드 200이면 성공)
     const newCategoryResult = await categoryService.newCategory(category);
