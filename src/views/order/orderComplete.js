27708a64ced2c9f2963189ad89392e51829b6199
unknown <sdawn0314@gmail.com>
2023-10-10 10:48:30 +0900

    update order complete page

--- src/views/order/orderComplete.js
+++ src/views/order/orderComplete.js
@@ -1,9 +1,8 @@
 let orderCompleteEl = `
-  <p>고객님이 주문하신 주문번호는
-  111 입니다.
-
-  주문내역 확인은 <a href="../user/orderTracking/orderTracking.html">주문조회</a>애서 가능합니다.
-  </p>
+  <span id="user-order-num">고객님이 주문하신 주문번호는
+  111 입니다.</span>
+  <span>주문내역 확인은 <a href="../user/orderTracking/orderTracking.html">주문조회</a>애서 가능합니다.
+  </span>
   `;
 
 const initorderComplete = () => {
