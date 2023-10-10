7dc4cf5f029d12d1d03027b37878b019510c181c
[SW6] 정찬영 <jcy8828@gmail.com>
2023-10-11 02:42:08 +0900

    상품상세페이지 장바구니버튼 구현

--- src/views/products/details/product-detail.js
+++ src/views/products/details/product-detail.js
@@ -20,7 +20,7 @@ function decreaseNumber() {
 arrowUp.addEventListener("click", increaseNumber);
 arrowDown.addEventListener("click", decreaseNumber);
 
-// 상품 데이터
+// 상품 데이터 불러오기
 const pathSegments = window.location.pathname.split("/");
 const productId = pathSegments[pathSegments.length - 1]; // 마지막 세그먼트를 가져옵니다.
 let product = {};
@@ -32,38 +32,44 @@ fetch(`/api/products/${productId}`)
     }
     return response.json();
   })
-  .then((product) => {
-    console.log(product);
-    document.querySelector(".product-name").textContent = product.productName;
-    document.querySelector(".product-price").textContent = product.price;
-    document.querySelector(".products-img").src = product.productImg;
-    document.querySelector(".productsDet-img").src = product.description;
+  .then((productData) => {
+    console.log(productData);
+    document.querySelector(".product-name").textContent =
+      productData.productName;
+    document.querySelector(".product-price").textContent = productData.price;
+    document.querySelector(".products-img").src = productData.productImg;
+    document.querySelector(".productsDet-img").src = productData.description;
+
+    product = {
+      productId: productData._id,
+      productName: productData.productName,
+      size: product.size,
+      quantity: 1, // 기본값으로 1 설정
+      price: productData.price,
+      productImg: productData.productImg,
+    };
   })
   .catch((error) => {
     console.error("Error", error);
   });
 
-const productSizeSelect = document.getElementById("productSize");
+const productSizeSelect = document.getElementById("product-size");
 
 document
   .querySelector(".add-to-cart-button")
   .addEventListener("click", function () {
-    // 사용자가 선택한 수량 가져오기
     product.quantity = parseInt(
       document.querySelector(".number").textContent,
       10
     );
 
-    // 사용자가 선택한 사이즈 가져오기
     product.size = productSizeSelect.value;
 
-    // 사용자가 선택하지 않았다면 경고메세지 출력
     if (product.size === "" || product.size === null) {
-      alert("Please select a size!");
+      alert("상품 사이즈를 선택해주세요.");
       return;
     }
 
-    // 로컬 스토리지에서 장바구니 정보를 불러옵니다.
     const cart = JSON.parse(localStorage.getItem("myCart")) || [];
 
     // 장바구니에 같은 상품이 이미 있는지 확인합니다.
@@ -75,12 +81,50 @@ document
       // 이미 있으면 수량을 업데이트합니다.
       existingProduct.quantity += product.quantity;
     } else {
-      // 없으면 장바구니에 추가합니다.
       cart.push(product);
     }
-
-    // 장바구니 정보를 로컬 스토리지에 저장합니다.
     localStorage.setItem("myCart", JSON.stringify(cart));
 
     alert("장바구니에 추가되었습니다.");
   });
+
+// 후기,문의 버튼 클릭 시 로그인 토큰 검사
+document.getElementById("reviews-btn").addEventListener("click", function () {
+  console.log("Reviews button clicked");
+  verifyTokenAndRedirect(
+    "/products/details/products-detail-form.html?type=review"
+  );
+});
+
+document.getElementById("questions-btn").addEventListener("click", function () {
+  verifyTokenAndRedirect(
+    "/products/details/products-detail-form.html?type=question"
+  );
+});
+
+function verifyTokenAndRedirect(url) {
+  const token = localStorage.getItem("Authorization") || "";
+  if (!token) {
+    alert("로그인이 필요합니다.");
+    console.log("Redirecting to login page...");
+    window.location.href = "/login";
+    return;
+  }
+
+  fetch("/api/products/{productId}", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.location.href = url;
+      } else {
+        alert("세션이 만료되었습니다. 다시 로그인해주세요.");
+        window.location.href = "/login";
+      }
+    })
+    .catch((err) => console.log(err));
+}
