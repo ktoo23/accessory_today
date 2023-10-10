ce35f1688657d1535f009f9204e29404fdf2abec
Dawon Shin <shindw0314@gmail.com>
2023-10-10 23:34:16 +0900

    fix products

--- src/routers/viewRouter.js
+++ src/routers/viewRouter.js
@@ -10,11 +10,6 @@ viewsRouter.get("/", (req, res) => {
   res.sendFile(path.join(process.cwd(), "src", "views", "public", "main.html"));
 });
 
-// About
-viewsRouter.get("/about", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "src", "views", "public", "about.html"));
-});
-
 // 장바구니
 viewsRouter.get("/cart", (req, res) => {
   res.sendFile(path.join(process.cwd(), "src", "views", "cart", "cart.html"));
@@ -40,24 +35,9 @@ viewsRouter.get("/order", (req, res) => {
 // 상품 페이지
 viewsRouter.get("/products", (req, res) => {
   res.sendFile(
-    path.join(process.cwd(), "src", "views", "products", "product.html")
+    path.join(process.cwd(), "src", "views", "products", "products.html")
   );
 });
-
-// 유저 정보 변경 페이지
-viewsRouter.get("/user/userinfo/:userId", (req, res) => {
-  res.sendFile(
-    path.join(
-      process.cwd(),
-      "src",
-      "views",
-      "user",
-      "userInfo",
-      "userInfo.html"
-    )
-  );
-});
-
 
 // 상품 디테일 페이지
 viewsRouter.get("/products/details", (req, res) => {
@@ -91,20 +71,6 @@ viewsRouter.get("/user/orderTracking", (req, res) => {
       "user",
       "orderTracking",
       "orderTracking.html"
-    )
-  );
-});
-
-// 비회원 주문 조회
-viewsRouter.get("/non-member/order-tracking/:orderId", (req, res) => {
-  res.sendFile(
-    path.join(
-      process.cwd(),
-      "src",
-      "views",
-      "user",
-      "nonMemberPage",
-      "nonMemberPage.html"
     )
   );
 });
